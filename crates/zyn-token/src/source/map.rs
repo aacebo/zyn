4b3ea10ee6f9dd5c5de5391aeec80eac7bc96451
aacebo <aacebowork@gmail.com>
2026-03-13 20:10:09 -0400

    Update map.rs

--- crates/zyn-token/src/source/map.rs
+++ crates/zyn-token/src/source/map.rs
@@ -1,9 +1,13 @@
-use crate::SourceFile;
+use crate::{SourceFile, Span};
 
 #[derive(Debug, Default)]
 pub struct SourceMap(Vec<SourceFile>);
 
 impl SourceMap {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
     pub fn files(&self) -> &[SourceFile] {
         &self.0
     }
@@ -13,6 +17,44 @@ impl SourceMap {
     }
 
     pub fn is_empty(&self) -> bool {
-        self.len() == 0
+        self.0.is_empty()
+    }
+
+    pub fn find(&self, span: Span) -> Option<&SourceFile> {
+        match self.find_index(span) {
+            None => None,
+            Some(i) => Some(&self.0[i]),
+        }
+    }
+
+    pub fn find_mut(&mut self, span: Span) -> Option<&mut SourceFile> {
+        match self.find_index(span) {
+            None => None,
+            Some(i) => Some(&mut self.0[i]),
+        }
+    }
+
+    pub fn find_path(&self, span: Span) -> Option<String> {
+        if let Some(i) = self.find_index(span) {
+            return if i == 0 {
+                Some("<unspecified>".to_owned())
+            } else {
+                Some(format!("<parsed string {}>", i))
+            };
+        }
+
+        None
+    }
+
+    pub fn find_index(&self, span: Span) -> Option<usize> {
+        self.0.binary_search_by(|file| file.span().cmp(&span)).ok()
+    }
+
+    pub fn add(&mut self, src: impl Into<String>) -> Span {
+        let start = self.0.last().map(|file| file.span().end()).unwrap_or(0);
+        let file = SourceFile::new(start, src);
+        let span = file.span();
+        self.0.push(file);
+        span
     }
 }
